00c181e38385c1ba729503d8e4236d5d6d44a8d6
gurubluocean <100664924+gurubluocean@users.noreply.github.com>
2023-03-28 12:04:09 +0530

    Update cleaner.py

--- s3datacleaner/cleaner.py
+++ s3datacleaner/cleaner.py
@@ -1,11 +1,11 @@
 import pandas as pd
+import io
 import boto3
 
-def clean_dataset_from_s3(bucket_name, file_name, columns_to_drop):
+def clean_dataset_from_s3(s3_client, bucket_name, file_name, columns_to_drop):
     # Download dataset from S3
-    s3 = boto3.resource('s3')
-    obj = s3.Object(bucket_name, file_name)
-    body = obj.get()['Body'].read().decode('utf-8')
+    response = s3_client.get_object(Bucket=bucket_name, Key=file_name)
+    body = response['Body'].read().decode('utf-8')
     data = pd.read_csv(io.StringIO(body))
 
     # Drop columns
